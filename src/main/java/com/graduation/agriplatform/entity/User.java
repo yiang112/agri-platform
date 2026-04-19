6120da7b580b5be71e0848b57ca1ea078bba8f48
yiang112 <161546625+yiang112@users.noreply.github.com>
2026-04-20 00:57:40 +0800

    Create User.java

---
+++ src/main/java/com/graduation/agriplatform/entity/User.java
@@ -0,0 +1,18 @@
+package com.graduation.agriplatform.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("sys_user")
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String realName;
+    private String roleCode;
+    private Integer status;
+}
